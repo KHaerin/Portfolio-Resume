c07b73577f8a15df6d06e97cc2ceadc5ff85c47b
KHaerin <coleuyan55@gmail.com>
2024-01-28 14:08:06 +0800

    url pdf

--- abouts.js
+++ abouts.js
@@ -1,20 +1,7 @@
 document.addEventListener('DOMContentLoaded', function() {
   document.querySelector('.cvBtn').addEventListener('click', function(event) {
-    var pdfUrl = 'myCV/UyanCV.pdf';
+    var pdfUrl = ('https://blush-juliet-82.tiiny.site');
 
-    
-    var link = document.createElement('a');
-    link.href = pdfUrl;
-    link.target = '_blank'; 
-    link.download = 'Uyan.pdf'; 
-  
-   
-    document.body.appendChild(link);
-  
-  
-    link.click();
-  
-    
-    document.body.removeChild(link);
+    window.open(pdfUrl);
   });
 });
