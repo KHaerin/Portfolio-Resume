90ddd005ef24bf0dc90480ee98433b62401d7644
KHaerin <coleuyan55@gmail.com>
2024-01-28 13:52:44 +0800

    dl pdf

--- abouts.js
+++ abouts.js
@@ -1,17 +1,20 @@
 document.addEventListener('DOMContentLoaded', function() {
   document.querySelector('.cvBtn').addEventListener('click', function(event) {
-    var fileId = '1nP0aL9bswgc0R5t4ebEZpXB28LSWen9E';  // Replace with your actual file ID
-    var embedUrl = 'https://drive.google.com/file/d/' + fileId + '/preview';
+    var pdfUrl = 'myCV/UyanCV.pdf';
 
-    var iframe = document.createElement('iframe');
-    iframe.src = embedUrl;
-    iframe.style.width = '100%';
-    iframe.style.height = '600px';  // Adjust the height as needed
-
-    var modal = document.createElement('div');
-    modal.classList.add('pdf-modal');
-    modal.appendChild(iframe);
-
-    document.body.appendChild(modal);
+    
+    var link = document.createElement('a');
+    link.href = pdfUrl;
+    link.target = '_blank'; 
+    link.download = 'Uyan.pdf'; 
+  
+   
+    document.body.appendChild(link);
+  
+  
+    link.click();
+  
+    
+    document.body.removeChild(link);
   });
 });
