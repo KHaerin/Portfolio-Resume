12077f5eeee0672e171a3de7b718b89918f0cf91
KHaerin <coleuyan55@gmail.com>
2024-01-28 13:25:33 +0800

    CV DL

--- abouts.js
+++ abouts.js
@@ -2,11 +2,13 @@ document.addEventListener('DOMContentLoaded', function() {
     document.querySelector('.cvBtn').addEventListener('click', viewCVPDF);
   
     function viewCVPDF() {
-      var pdfFile = 'CV/UyanCV.pdf'
+      var fileId = '1nP0aL9bswgc0R5t4ebEZpXB28LSWen9E';
+      var downloadLink = 'https://drive.google.com/uc?id=' + fileId;
       var link = document.createElement('a');
-      link.href = pdfFile;
+      link.href = downloadLink;
       link.download = 'UyanCV.pdf';
       link.click();
       window.open(pdfFile, '_blank');
     }
   });
+
