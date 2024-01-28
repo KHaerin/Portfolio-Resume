3e28d4f9a05a11c85ff65f734dd52e3912396328
KHaerin <coleuyan55@gmail.com>
2024-01-28 13:33:35 +0800

    cv view

--- abouts.js
+++ abouts.js
@@ -1,14 +1,20 @@
 document.addEventListener('DOMContentLoaded', function() {
-    document.querySelector('.cvBtn').addEventListener('click', viewCVPDF);
-  
-    function viewCVPDF() {
-      var fileId = '1nP0aL9bswgc0R5t4ebEZpXB28LSWen9E';
-      var downloadLink = 'https://drive.google.com/uc?id=' + fileId;
-      var link = document.createElement('a');
-      link.href = downloadLink;
-      link.download = 'UyanCV.pdf';
-      link.click();
-      window.open(downloadLink, '_blank');
-    }
-  });
+  document.querySelector('.cvBtn').addEventListener('click', function(event) {
+      event.preventDefault(); 
 
+      var fileId = '1nP0aL9bswgc0R5t4ebEZpXB28LSWen9E';  
+      var downloadLink = 'https://drive.google.com/uc?id=' + fileId;
+
+      var xhr = new XMLHttpRequest();
+      xhr.open('GET', downloadLink, true);
+      xhr.responseType = 'blob';
+
+      xhr.onload = function () {
+          var blob = new Blob([xhr.response], { type: 'application/pdf' });
+          var url = URL.createObjectURL(blob);
+          window.open(url, '_blank');
+      };
+
+      xhr.send();
+  });
+});
